612f14fb04ae08c64edd99c177fcb3798dd145f0
abraham-DBA <irootezekiel318@gmail.com>
2026-01-23 13:28:33 -0800

    implement integration section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/landing/nav";
 import Hero from "@/components/landing/hero";
 import SocialProof from "@/components/landing/social";
 import Features from "@/components/landing/features";
+import Integration from "@/components/landing/integration";
 
 const Page = () => {
     return (
@@ -11,6 +12,7 @@ const Page = () => {
             <Hero />
             <SocialProof />
             <Features />
+            <Integration />
         </main>
     );
 };
