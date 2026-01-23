d3526e3a62c342b3d3e5deca8a25d86471c9073d
abraham-DBA <irootezekiel318@gmail.com>
2026-01-23 09:36:31 -0800

    add social proof section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "@/components/landing/nav";
 import Hero from "@/components/landing/hero";
+import SocialProof from "@/components/landing/social";
 
 const Page = () => {
     return (
         <main className="w-full flex flex-col relative z-10">
             <Navbar />
             <Hero />
+            <SocialProof />
         </main>
     );
 };
